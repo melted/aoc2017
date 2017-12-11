076fcd3b1dbf7b2e7d1bb7c85c5e8472ab511088
Niklas Larsson <metaniklas@gmail.com>
2017-12-11 23:22:04 +0100

    day 11 refactor

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,43 +1,25 @@
 use std::io::*;
 use std::cmp::max;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum Dir {
-    N, NW, NE, S, SW, SE
-}
-
-use Dir::*;
-
-fn load_data() -> Vec<Dir> {
+fn load_data() -> Vec<(i32, i32)> {
     let mut input = String::new();
     let mut out = Vec::new();
     let _ = stdin().read_to_string(&mut input).unwrap();
     for l in input.split(',') {
-        out.push(get_direction(l));
+        out.push(get_move(l));
     }
     out
 }
 
-fn get_direction(s : &str) -> Dir {
+fn get_move(s : &str) -> (i32, i32) {
     match s.trim() {
-        "n" => N,
-        "nw" => NW,
-        "ne" => NE,
-        "s" => S,
-        "sw" => SW,
-        "se" => SE,
-        c => { println!("{}", c); N }
-    }
-}
-
-fn get_move(d : &Dir) -> (i32, i32) {
-    match *d {
-        N => (0,2),
-        NW => (-2,1),
-        NE => (2,1),
-        S => (0,-2),
-        SW => (-2,-1),
-        SE => (2,-1)
+        "n" => (0,2),
+        "nw" => (-2,1),
+        "ne" => (2,1),
+        "s" => (0,-2),
+        "sw" => (-2,-1),
+        "se" => (2,-1),
+        _ => { println!("{}", s); (0,0)}
     }
 }
 
@@ -52,11 +34,13 @@ fn distance(x : i32, y : i32) -> i32 {
 
 fn main() {
     let v = load_data();
-    let end = v.iter().map(get_move).fold((0,0,0), |a, b| {
-        let x = a.0 + b.0;
-        let y = a.1 + b.1;
-        let d = distance(x,y);
-        (x, y, max(a.2, d))
-    });
-    println!("{} {:?}", distance(end.0, end.1), end.2);
+    let mut x = 0;
+    let mut y = 0;
+    let mut max_dist = 0;
+    for &(dx, dy) in v.iter() {
+        x += dx;
+        y += dy;
+        max_dist = max(max_dist, distance(x, y));
+    }
+    println!("{} {}", distance(x, y), max_dist);
 }
