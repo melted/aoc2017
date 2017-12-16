5dab2a4f738e70ddd354f7d9c52ec3eaca1223b8
Niklas Larsson <niklas@mm.st>
2017-12-16 16:15:11 +0100

    day 16 - part 2

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,11 +1,12 @@
 #![feature(slice_rotate)]
 use std::io::*;
+use std::collections::HashMap;
 
 #[derive(Clone, Copy, Debug)]
 enum Move {
     Spin(usize),
     Exchange(usize, usize),
-    Partner(u8, u8)
+    Partner(usize, usize)
 }
 
 use Move::*;
@@ -16,9 +17,9 @@ fn get_move(s : &str) -> Move {
         "s" => Spin(rest.parse().unwrap()),
         "p" => {
             let mut iter = rest.bytes();
-            let a = iter.next().unwrap();
+            let a = (iter.next().unwrap() - 97) as usize;
             let _ = iter.next();
-            let b = iter.next().unwrap();
+            let b = (iter.next().unwrap() - 97) as usize;
             Partner(a, b)
         },
         "x" => {
@@ -41,16 +42,16 @@ fn load_data() -> Vec<Move> {
     out
 }
 
-fn spin(s : &mut [u8], n : usize) {
+fn spin(s : &mut Vec<usize>, n : usize) {
     let l = s.len();
     s.rotate(l - n);
 }
 
-fn exchange(s : &mut [u8], a : usize, b : usize) {
+fn exchange(s : &mut Vec<usize>, a : usize, b : usize) {
     s.swap(a,b);
 }
 
-fn partner(s : &mut [u8], a : u8, b : u8) {
+fn partner(s : &mut Vec<usize>, a : usize, b : usize) {
     let mut pos_a = 0;
     let mut pos_b = 0;
     for i in 0..s.len() {
@@ -64,22 +65,52 @@ fn partner(s : &mut [u8], a : u8, b : u8) {
     s.swap(pos_a, pos_b);
 }
 
-fn dance(s : &String, moves : &Vec<Move>) -> String {
-    let mut temp = s.clone();
-    let mut out = unsafe { temp.as_bytes_mut() };
+fn dance(start : &Vec<usize>, moves : &Vec<Move>) -> Vec<usize> {
+    let mut out = start.clone();
     for &m in moves.iter() {
         match m {
-            Spin(n) => spin(out, n),
-            Exchange(a, b) => exchange(out, a, b),
-            Partner(a, b) => partner(out, a, b)
+            Spin(n) => spin(&mut out, n),
+            Exchange(a, b) => exchange(&mut out, a, b),
+            Partner(a, b) => partner(&mut out, a, b)
         }
     }
-    String::from_utf8(out.to_vec()).unwrap()
+    out
+}
+
+fn dance_rounds(start : &Vec<usize>, moves : &Vec<Move>, target : usize) -> Vec<usize> {
+    let mut out = start.clone();
+    let mut memo = HashMap::new();
+    memo.insert(start.clone(), 0);
+    for i in 1..target+1 {
+        let next = dance(&out, moves);
+        if memo.contains_key(&next) {
+            let s = memo.get(&next).unwrap();
+            let period = i - s;
+            let new_target = (target - s) % period + s;
+            return dance_rounds(start, moves, new_target);
+        } else {
+            memo.insert(next.clone(), i);
+        }
+        out = next;
+    }
+    out
+}
+
+fn order_to_string(s : &str, order : &Vec<usize>) -> String {
+    let mut out = String::new();
+    let chars : Vec<char> = s.chars().collect();
+    for &i in order.iter() {
+        out.push(chars[i]);
+    }
+    out
 }
 
 fn main() {
-    let mut start = String::from("abcdefghijklmnop");
+    let start = String::from("abcdefghijklmnop");
+    let init = vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
     let v = load_data();
-    let finish = dance(&start, &v);
-    println!("{:?}", finish);
+    let a = dance_rounds(&init,&v,1);
+    println!("{}", order_to_string(&start, &a));
+    let b = dance_rounds(&init, &v, 1000000000);
+    println!("{}", order_to_string(&start, &b));
 }
